db06099eed2d2a1f70b19c87b43d62506e90ac8a
ss77995ss <ss77995ss@gmail.com>
2019-08-17 01:47:28 +0800

    Change folders structure

---
+++ src/client/components/home/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Home = () => <div id="home-wrapper">Hello World</div>
+
+export default Home
---
+++ src/client/test/components/home/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import expect from 'expect'
+
+import Home from 'components/home'
+
+describe('<Home />', () => {
+  it('should render correctly', () => {
+    const wrapper = shallow(<Home />)
+    expect(wrapper.find('#home-wrapper').length).toBe(true)
+  })
+})
