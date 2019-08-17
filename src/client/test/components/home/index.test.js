6f05c46606c6e38cf4e358fe69878cce3453be19
ss77995ss <ss77995ss@gmail.com>
2019-08-17 13:08:33 +0800

    Add enzyme adapter to make unit test success

--- src/client/test/components/home/index.test.js
+++ src/client/test/components/home/index.test.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import expect from 'expect'
 
-import Home from 'components/home'
+import Home from '../../../components/home'
 
 describe('<Home />', () => {
   it('should render correctly', () => {
     const wrapper = shallow(<Home />)
-    expect(wrapper.find('#home-wrapper').length).toBe(true)
+    expect(wrapper.find('#home-wrapper').length).toBe(1)
   })
 })
