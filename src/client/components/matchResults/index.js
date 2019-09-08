187df1aebd75af2c623dac40331f8b755cbe4446
ss77995ss <ss77995ss@gmail.com>
2019-09-08 23:13:47 +0800

    Add button to switch different context

--- src/client/components/matchResults/index.js
+++ src/client/components/matchResults/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import Fade from '@material-ui/core/Fade';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
 
-const MatchResults = () => (
-  <div className="match-results-wrapper">
-    <h1>Baseball Match Results</h1>
-    <Button variant="contained">Scorekeeping by Innings</Button>
-  </div>
-);
+const useStyles = makeStyles({
+  matchResultsWrapper: {
+    textAlign: 'center'
+  }
+});
+
+const MatchResults = () => {
+  const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(true);
+  const handleClick = useCallback(() => setIsOpen(!isOpen), [isOpen]);
+
+  return (
+    <div className={classes.matchResultsWrapper}>
+      {isOpen ? (
+        <Fade in={isOpen}>
+          <Box>
+            <Typography variant="h4" component="h1">
+              Baseball Match Results
+            </Typography>
+            <Button variant="contained" onClick={handleClick}>
+              Scorekeeping by Innings
+            </Button>
+          </Box>
+        </Fade>
+      ) : (
+        <Button variant="contained" onClick={handleClick}>
+          Back
+        </Button>
+      )}
+    </div>
+  );
+};
 
 export default MatchResults;
