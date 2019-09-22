33be3a38cd4ec611b8ff1688f83dc3624d965d85
ss77995ss <ss77995ss@gmail.com>
2019-09-22 16:15:32 +0800

    Add basic content of <BatterStats /> component

--- src/client/components/matchResults/scoreKeepByInnings/inningsSelector.js
+++ src/client/components/matchResults/scoreKeepByInnings/inningsSelector.js
@@ -3,6 +3,8 @@ import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import AddIcon from '@material-ui/icons/Add';
 
+import BatterStats from './batterStats';
+
 const renderInnings = innings => {
   const inningsTabs = Array.from({ length: innings }, (v, inning) => (
     <Tab key={`innings-#${inning}`} label={inning + 1} />
@@ -18,10 +20,13 @@ const InningsSelector = () => {
   const handleChange = (event, value) => setTabIndex(value);
 
   return (
-    <Tabs value={tabIndex} onChange={handleChange}>
-      {renderInnings(innings)}
-      <Tab icon={<AddIcon />} onClick={handleClick} />
-    </Tabs>
+    <div>
+      <Tabs value={tabIndex} onChange={handleChange}>
+        {renderInnings(innings)}
+        <Tab icon={<AddIcon />} onClick={handleClick} />
+      </Tabs>
+      <BatterStats />
+    </div>
   );
 };
 
--- src/client/constants/base.js
+++ src/client/constants/base.js
@@ -5,5 +5,14 @@ export const titles = {
 
 export const buttonText = {
   backToHome: 'Back to Home page',
+  batterStat: {
+    cancel: 'Cancel',
+    save: 'Save'
+  },
   scoreKeepByInning: 'Scorekeeping by Innings'
+};
+
+export const battersConfig = {
+  defaultBatters: 3,
+  battersInARow: 9
 };
