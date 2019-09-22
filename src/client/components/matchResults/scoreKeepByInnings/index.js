25c0a968319ea9201f31bc1232fc872df9f59dae
ss77995ss <ss77995ss@gmail.com>
2019-09-22 11:47:19 +0800

    Add inning selector to let user select innings

--- src/client/components/matchResults/scoreKeepByInnings/index.js
+++ src/client/components/matchResults/scoreKeepByInnings/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Fade from '@material-ui/core/Fade';
 import { Button } from '@material-ui/core';
+import Fade from '@material-ui/core/Fade';
+import Typography from '@material-ui/core/Typography';
 
 import { titles, buttonText } from 'client/constants/base';
+import InningsSelector from './inningsSelector';
 
 const ScoreKeepByInnings = ({ isScoreKeepOpen, onClick }) => (
   <Fade in={isScoreKeepOpen}>
@@ -13,6 +14,8 @@ const ScoreKeepByInnings = ({ isScoreKeepOpen, onClick }) => (
       <Typography variant="h4" component="h1">
         {titles.scoreKeepByInning}
       </Typography>
+      <InningsSelector />
+      <hr />
       <Button variant="contained" onClick={onClick}>
         {buttonText.backToHome}
       </Button>
---
+++ src/client/components/matchResults/scoreKeepByInnings/inningsSelector.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import AddIcon from '@material-ui/icons/Add';
+
+const renderInnings = innings => {
+  const inningsTabs = Array.from({ length: innings }, (v, inning) => (
+    <Tab key={`innings-#${inning}`} label={inning + 1} />
+  ));
+
+  return inningsTabs;
+};
+
+const InningsSelector = () => {
+  const [innings, setInnings] = useState(1);
+  const [tabIndex, setTabIndex] = useState(0);
+  const handleClick = () => setInnings(innings + 1);
+  const handleChange = (event, value) => setTabIndex(value);
+
+  return (
+    <Tabs value={tabIndex} onChange={handleChange}>
+      {renderInnings(innings)}
+      <Tab icon={<AddIcon />} onClick={handleClick} />
+    </Tabs>
+  );
+};
+
+export default InningsSelector;
